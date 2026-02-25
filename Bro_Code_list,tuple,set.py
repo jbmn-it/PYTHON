80bd9e4f18afba773bd16ef85f125532b5b9120b
Joy Barmon <barmonjoy06@gmail.com>
2026-02-25 17:24:56 +0600

    Update Bro_Code_list,tuple,set.py
    comment is ok

--- Bro_Code_list,tuple,set.py
+++ Bro_Code_list,tuple,set.py
@@ -1,43 +1,43 @@
-# collection = single "variable" used to stote multiple values
-#   list = [] ordered and changeable. Duplicates OK
-#   set = {} unordered and immutable, but Add/Remove OK. NO dublicates
-#   tuple= () ordered and unchangeable. Duplicates OK. Faster
+collection = single "variable" used to stote multiple values
+  list = [] ordered and changeable. Duplicates OK
+  set = {} unordered and immutable, but Add/Remove OK. NO dublicates
+  tuple= () ordered and unchangeable. Duplicates OK. Faster
 
-# fruits = ("Apple", "Orange", "Banana", "Coconut", "Pineapple", "Mango", "Apple")
+fruits = ("Apple", "Orange", "Banana", "Coconut", "Pineapple", "Mango", "Apple")
 
-# list
+list
 
-# print(dir(fruits))
-# print(help(fruits))
-# print(len(fruits))
-# print("Mango" in fruits)
+print(dir(fruits))
+print(help(fruits))
+print(len(fruits))
+print("Mango" in fruits)
 
-# fruits[1] = "lychee"
-# fruits.append("Jack-fruit")
-# fruits.remove("Apple")
-# fruits.insert(0, "Guava")
-# fruits.sort()
-# fruits.reverse()
-# fruits.clear()
-# print(fruits.index("Mango"))
-# print(fruits.count("Apple"))
+fruits[1] = "lychee"
+fruits.append("Jack-fruit")
+fruits.remove("Apple")
+fruits.insert(0, "Guava")
+fruits.sort()
+fruits.reverse()
+fruits.clear()
+print(fruits.index("Mango"))
+print(fruits.count("Apple"))
 
-# print(fruits)
-# for fruit in fruits:
-#     print(fruit)
+print(fruits)
+for fruit in fruits:
+    print(fruit)
 
-# set
+set
 
-# print("Guava" in fruits)
-# fruits.add("apple")
-# fruits.pop()
-# print(fruits)
+print("Guava" in fruits)
+fruits.add("apple")
+fruits.pop()
+print(fruits)
 
-# tuple
+tuple
 
-# print(fruits.count("Apple"))
-# print(fruits.index("Mango"))
-# print(dir(fruits))
-# for fruit in fruits:
-#     print(fruit)
+print(fruits.count("Apple"))
+print(fruits.index("Mango"))
+print(dir(fruits))
+for fruit in fruits:
+    print(fruit)
   
