ac8d50a27568e3c694fe291a2827ca1be9541e9b
Joy Barmon <barmonjoy06@gmail.com>
2026-02-25 17:28:36 +0600

    Update Python_2D_Collection.py
    has changed!

--- Python_2D_Collection.py
+++ Python_2D_Collection.py
@@ -1,21 +1,21 @@
-# groceries = (("apple", "banana", "mango"),
-#             ("celery", "potatoes", "carrots"),
-#             ("chiken", "fish", "turky"))
+groceries = (("apple", "banana", "mango"),
+            ("celery", "potatoes", "carrots"),
+            ("chiken", "fish", "turky"))
 
-# for collection in groceries:
-#     for food in collection:
-#         print(food, end=" ")
+for collection in groceries:
+    for food in collection:
+        print(food, end=" ")
 
-#     print()   
+    print()   
 
 
-# Excercise
+Excercise
 
-# num =((1,2,3),
-#      (4,5,6),
-#      (6,7,8),
-#      ("*",0,9))
-# for x in num:
-#     for y in x:
-#         print(y, end=" ")
-#     print()       
+num =((1,2,3),
+     (4,5,6),
+     (6,7,8),
+     ("*",0,9))
+for x in num:
+    for y in x:
+        print(y, end=" ")
+    print()       
